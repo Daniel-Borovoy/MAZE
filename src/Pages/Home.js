966794689b895feb0c9ccdc47a3323730d1b96f1
TheGrids <72039184+TheGrids@users.noreply.github.com>
2021-03-25 20:48:32 +0300

    Footer

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,7 @@
 import { Carousel } from 'bootstrap'
 import React, { Component } from 'react'
-import { Container, Row } from 'react-bootstrap'
-import CarouselBox from '../assets/CarouselBox'
+import { Container, Row, Jumbotron } from 'react-bootstrap'
+import FooterPage from '../assets/FooterPage'
 import Img from '../assets/1.jpg'
 export default class Home extends Component {
     render() {
@@ -17,6 +17,15 @@ export default class Home extends Component {
                 <h1 className="col-6 m-auto">Just improve yourself</h1>    
                 </Row>
             </Container>  
+            <Jumbotron fluid>
+                <Container>
+                    <h1>MAZE</h1>
+                    <p>
+                    Лишь предприниматели в сети интернет подвергнуты целой серии независимых исследований. Мы вынуждены отталкиваться от того, что выбранный нами инновационный путь однозначно определяет каждого участника как способного принимать собственные решения касаемо системы массового участия. Равным образом, укрепление и развитие внутренней структуры не даёт нам иного выбора, кроме определения системы массового участия!
+                    </p>
+                </Container>
+            </Jumbotron>
+            <FooterPage/>
         </>
         )
     }
